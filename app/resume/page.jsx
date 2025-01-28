1975eee61f774319fc36a7aad23c6487b8934665
Ahmadalbouchi <aalbouch@my.centennialcollege.ca>
2025-01-28 15:43:06 -0500

    make the experience tap default

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -201,13 +201,13 @@ const Resume = () => {
           initial={{ opacity: 0 }}
           animate={{ 
             opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+            transition: { delay: 0.4, duration: 0.4, ease: "easeIn" }
           }}
           className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
         >
         <div className="container mx-auto ">
             <Tabs
-            defaultValue="experince"
+            defaultValue="experience"
           className="flex flex-col xl:flex-row gap-[60px]"
             >
             <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
