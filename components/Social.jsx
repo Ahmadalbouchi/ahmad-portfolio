69b403fe8bf1e79f197c443a40fce5b40283f711
Ahmadalbouchi <aalbouch@my.centennialcollege.ca>
2025-01-28 14:23:13 -0500

    adjsut the Social buttons icon

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter, FaWhatsapp  } from "react-icons/fa";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaWhatsapp />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  { icon: <FaGithub />, path: " https://github.com/Ahmadalbouchi" },
+  { icon: <FaLinkedinIn />, path: "  https://www.linkedin.com/in/ahmad-al-bouchi/ " },
+  { icon: <FaWhatsapp />, path: "               https://wa.me/qr/3TZOB643FACOH1    " },
+  { icon: <FaTwitter />, path: " https://x.com/AhmadAlboushi2?s=08" },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
