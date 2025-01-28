95b05a7426302c444388fcbf31fd5ba00add5b91
Ahmadalbouchi <aalbouch@my.centennialcollege.ca>
2025-01-28 12:23:14 -0500

    adjust the send message button

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -51,50 +51,58 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
         {/* form */}
         <div className="xl:w-[54%] order-2 xl:order-none" > 
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+        <form
+  className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+  action="https://api.web3forms.com/submit"
+  method="POST"
+>
+  {/* Add the Web3Forms access key */}
+  <input type="hidden" name="access_key" value="e33224e3-72f7-456c-a4fb-f2070003c297" />
 
-                        <h3 className="text-4xl text-accent">Let's work together</h3>
-                        <p className="text-white/60">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum 
-                    nihil sapiente pariatur id totam.
-                    </p>
+  <h3 className="text-4xl text-accent">Let's work together</h3>
+  <p className="text-white/60">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nihil
+    sapiente pariatur id totam.
+  </p>
 
-                    {/* input */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-                    <Input type="firstname" placeholder="Firstname" />
-                    <Input type="lastname" placeholder="Lastname" />
-                    <Input type="email" placeholder="Email address" />
-                    <Input type="phone" placeholder="Phone number" />
-                    </div>
+  {/* Input fields */}
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <Input type="text" name="firstname" placeholder="Firstname" required />
+    <Input type="text" name="lastname" placeholder="Lastname" required />
+    <Input type="email" name="email" placeholder="Email address" required />
+    <Input type="tel" name="phone" placeholder="Phone number" />
+  </div>
 
-                   {/* select */}
-                    <Select>
-                    <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Select a service" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                        <SelectLabel>Select a service</SelectLabel>
-                        <SelectItem value="est">Web Development</SelectItem>
-                        <SelectItem value="cst">UI/UX Design</SelectItem>
-                        <SelectItem value="mst">Back-end Developer</SelectItem>
-                        <SelectItem value="mst">Database Developer</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                    </Select>
+  {/* Service selection */}
+  <Select name="service" required>
+    <SelectTrigger className="w-full">
+      <SelectValue placeholder="Select a service" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel>Select a service</SelectLabel>
+        <SelectItem value="Web Development">Web Development</SelectItem>
+        <SelectItem value="UI/UX Design">UI/UX Design</SelectItem>
+        <SelectItem value="Back-end Developer">Back-end Developer</SelectItem>
+        <SelectItem value="Database Developer">Database Developer</SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
 
-                    {/* textarea */}
-                    <Textarea 
-                    className="h-[200px]" 
-                    placeholder="Type your message here." 
-                    />
+  {/* Message text area */}
+  <Textarea
+    className="h-[200px]"
+    name="message"
+    placeholder="Type your message here."
+    required
+  />
 
-                    {/* btn */}
-                    <Button size="md" className="max-w-30">
-                    Send message
-                    </Button>
+  {/* Submit button */}
+  <Button size="md" className="max-w-30" type="submit">
+    Send message to my email
+  </Button>
+</form>
 
-                </form>
             </div>
 
         {/* info */}
