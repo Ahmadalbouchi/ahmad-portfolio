d1b4b7f1d1851583bf186b6e7fb55736b7a5ef29
Ahmadalbouchi <aalbouch@my.centennialcollege.ca>
2025-01-30 12:43:27 -0500

    service page configration

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,30 +2,43 @@
 
 import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
+import Contact from "../contact/page";
 
 const services = [
   {
     num: "01",
     title: "Web Development",
-    description: "here I should write the  description about this service but I will do it later on... it's services/page.jsx",
+    description: "Your vision, my expertise. I collaborate closely with you to understand your needs and create a website that reflects your brand identity and resonates with your target audience.  Let's build something amazing together.",
     href:"",
   },
   {
     num: "02",
     title: "Ui/Ux Design",
-    description: "here I should write the  description about this service but I will do it later on... it's services/page.jsx",
+    description: "From concept to pixel-perfect design, I guide you through the entire UI/UX process.  I'll help you define your target audience, create user flows, and develop a design system that ensures consistency and scalability.",
     href:"",
   },
   {
     num: "03",
-    title: "Database Developer",
-    description: "here I should write the  description about this service but I will do it later on... it's services/page.jsx",
+    title: "Database Development",
+    description: "Your data is your most valuable asset. I design and develop robust and reliable databases that ensure data integrity, security, and accessibility.  Let me help you unlock the power of your data.",
     href:"",
   },
   {
     num: "04",
-    title: "Backend Developer",
-    description: "here I should write the  description about this service but I will do it later on... it's services/page.jsx",
+    title: "Backend Development",
+    description: "I'm not just a developer, I'm your long-term technology partner. I provide ongoing support and maintenance for the backend systems I build, ensuring your applications remain stable, secure, and performant.  I'm here to help you scale and adapt your backend as your business grows.",
+    href:"",
+  },
+  {
+    num: "05",
+    title: "Software Development",
+    description: "From concept to deployment, I deliver robust and scalable software solutions tailored to your business needs. Proficient in Java, C#, and Python, I can build applications for a variety of platforms, including web, desktop, and mobile. Let's bring your software vision to life.",
+    href:"",
+  },
+  {
+    num: "06",
+    title: "Android Development",
+    description: "Building bridges between your vision and your audience. I create Android apps that foster meaningful connections, turning users into loyal fans.",
     href:"",
   },
 ];
